274358e7414d235d728b025e61352cd4980c4a08
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2019-09-22 15:19:20 -0400

    Added a testing button for the grid

--- 3Stone/src/presentation/GameViewFXMLController.java
+++ 3Stone/src/presentation/GameViewFXMLController.java
@@ -9,6 +9,8 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class GameViewFXMLController {
@@ -19,21 +21,34 @@ public class GameViewFXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="testLbl"
+    private Label testLbl; // Value injected by FXMLLoader
+
+    @FXML // fx:id="testBtn1"
+    private Button testBtn1; // Value injected by FXMLLoader
+
     @FXML // fx:id="gameGrid"
     private GridPane gameGrid; // Value injected by FXMLLoader
 
     @FXML
     void mouseEnterHandle(ActionEvent event) {
-        Node cellClicked = (Node)event.getSource();
-        Integer colIndex = GridPane.getColumnIndex(cellClicked);
-        Integer rowIndex = GridPane.getRowIndex(cellClicked);
-        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(),
-                rowIndex.intValue());
+    }
+
+    @FXML
+    void buttonHandler(ActionEvent event) {
+         System.out.println("I have been Clicked");
+            this.testLbl.setText("Clicked");
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert testLbl != null : "fx:id=\"testLbl\" was not injected: check your FXML file 'GameViewFXML.fxml'.";
+        assert testBtn1 != null : "fx:id=\"testBtn1\" was not injected: check your FXML file 'GameViewFXML.fxml'.";
         assert gameGrid != null : "fx:id=\"gameGrid\" was not injected: check your FXML file 'GameViewFXML.fxml'.";
 
+    }
+    
+    private void initGrid(){
+    
     }
 }
