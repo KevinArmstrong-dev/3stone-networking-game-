bdfc6ab6813b58db1dc2968291b5ae937cf936b5
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2019-09-22 15:37:21 -0400

    Populated the grid with buttons

--- 3Stone/src/presentation/GameViewFXMLController.java
+++ 3Stone/src/presentation/GameViewFXMLController.java
@@ -24,8 +24,8 @@ public class GameViewFXMLController {
     @FXML // fx:id="testLbl"
     private Label testLbl; // Value injected by FXMLLoader
 
-    @FXML // fx:id="testBtn1"
-    private Button testBtn1; // Value injected by FXMLLoader
+//    @FXML // fx:id="testBtn1"
+//    private Button testBtn1; // Value injected by FXMLLoader
 
     @FXML // fx:id="gameGrid"
     private GridPane gameGrid; // Value injected by FXMLLoader
@@ -34,21 +34,31 @@ public class GameViewFXMLController {
     void mouseEnterHandle(ActionEvent event) {
     }
 
-    @FXML
-    void buttonHandler(ActionEvent event) {
-         System.out.println("I have been Clicked");
-            this.testLbl.setText("Clicked");
-    }
+//    @FXML
+//    void buttonHandler(ActionEvent event) {
+//         System.out.println("I have been Clicked");
+//            this.testLbl.setText("Clicked");
+//    }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert testLbl != null : "fx:id=\"testLbl\" was not injected: check your FXML file 'GameViewFXML.fxml'.";
-        assert testBtn1 != null : "fx:id=\"testBtn1\" was not injected: check your FXML file 'GameViewFXML.fxml'.";
+//        assert testBtn1 != null : "fx:id=\"testBtn1\" was not injected: check your FXML file 'GameViewFXML.fxml'.";
         assert gameGrid != null : "fx:id=\"gameGrid\" was not injected: check your FXML file 'GameViewFXML.fxml'.";
 
+        initGrid();
     }
     
     private void initGrid(){
-    
+        for(int row = 0; row < 11; row++){
+            for(int col = 0; col < 11 ; col++){
+                Button stone= new Button(row+""+col);
+                stone.setOnAction(event -> {
+                    String pos = "Clicked "+stone;
+                    this.testLbl.setText(pos);
+                });
+                gameGrid.add(stone, col, row);
+            }
+        }
     }
 }
