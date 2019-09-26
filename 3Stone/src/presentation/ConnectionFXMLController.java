8a36a559811586cd0794fb9868bfc107177c63ca
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2019-09-26 18:37:48 -0400

    Added Two windows and connect button method handler

--- 3Stone/src/presentation/ConnectionFXMLController.java
+++ 3Stone/src/presentation/ConnectionFXMLController.java
@@ -5,9 +5,15 @@
 package presentation;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class ConnectionFXMLController {
 
@@ -17,12 +23,47 @@ public class ConnectionFXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="connectBtn"
+    private Button connectBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ipTxt"
+    private TextField ipTxt; // Value injected by FXMLLoader
+
+    @FXML // fx:id="exitBtn"
+    private Button exitBtn; // Value injected by FXMLLoader
+
     @FXML // fx:id="serverIpLbl"
     private Label serverIpLbl; // Value injected by FXMLLoader
+    
+    private List<Pane> panes;
+
+    @FXML
+    void connectBtnHandler(ActionEvent event) {
+        System.out.println("the size of panes is " + panes.size());
+        //This hides the welcome Pane
+        panes.get(0).setVisible(false);
+        
+        //This displays the game window
+        panes.get(1).setVisible(true);
+    }
+
+    @FXML
+    void exitBtnHandler(ActionEvent event) {
+        //Close the App
+        System.out.println("Closing the Application");
+        Platform.exit();
+    }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert connectBtn != null : "fx:id=\"connectBtn\" was not injected: check your FXML file 'ConnectionFXML.fxml'.";
+        assert ipTxt != null : "fx:id=\"ipTxt\" was not injected: check your FXML file 'ConnectionFXML.fxml'.";
+        assert exitBtn != null : "fx:id=\"exitBtn\" was not injected: check your FXML file 'ConnectionFXML.fxml'.";
         assert serverIpLbl != null : "fx:id=\"serverIpLbl\" was not injected: check your FXML file 'ConnectionFXML.fxml'.";
 
+    }
+    
+    public void getPaneList(List panes){
+        this.panes = panes;
     }
 }
--- 3Stone/src/presentation/RootFXMLController.java
+++ 3Stone/src/presentation/RootFXMLController.java
@@ -4,9 +4,16 @@
 
 package presentation;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 public class RootFXMLController {
@@ -19,13 +26,59 @@ public class RootFXMLController {
 
     @FXML // fx:id="rootPane"
     private StackPane rootPane; // Value injected by FXMLLoader
+    
+    private List<Pane> paneList= new ArrayList<Pane>();
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert rootPane != null : "fx:id=\"rootPane\" was not injected: check your FXML file 'RootFXML.fxml'.";
-
+        initWelcomeWindow();
+        initGameView();
     
     }
+    private void initWelcomeWindow(){
+      
+        try {
+            FXMLLoader loader = new FXMLLoader();           
+            loader.setResources(resources);
+
+            loader.setLocation(MainApp.class
+                    .getResource("connectionFXML.fxml"));
+            AnchorPane connectionGui = loader.load();
+             paneList.add(connectionGui);
+            // Give the controller the data object.
+            ConnectionFXMLController connectionPane = loader.getController();
+            connectionPane.getPaneList(paneList);
+            rootPane.getChildren().add(connectionGui);
+            
+        } catch (IOException ex) {
+            System.out.println("Error loading the welcome Pane");
+            Platform.exit();
+        }
+    }
     
+    private void initGameView(){
+       
+        try {
+            FXMLLoader loader = new FXMLLoader();           
+            loader.setResources(resources);
+
+            loader.setLocation(MainApp.class
+                    .getResource("GameViewFXML.fxml"));
+            AnchorPane gamePane = loader.load();
+            
+            gamePane.setVisible(false);
+            
+            paneList.add(gamePane);
+            // Give the controller the data object.
+            GameViewFXMLController gameController = loader.getController();
+              
+            rootPane.getChildren().add(gamePane);
+            
+        } catch (IOException ex) {
+            System.out.println("Game pane error");
+            Platform.exit();
+        }
+    }
     
 }
